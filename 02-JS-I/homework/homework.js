6775d17e07cbbd16a8cb966c5aa3ecc35fae87c1
Marcelo <marcemijlin@gmail.com>
2022-07-20 12:46:59 -0300

    third commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -76,7 +76,7 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  if(str1.length = str2.length){
+  if(str1.length == str2.length){
     return true;
   }
     return false;
@@ -186,7 +186,7 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  var saludo = "Hola" + " " + nombre;
+  var saludo = "Hola" + " " + nombre + "!";
   return saludo;
 }
 
